3ac08e61afca96ba4c80edf594d899aa16e023c2
Pierre Lannoy <pierre@lannoy.fr>
2022-01-26 17:49:08 +0100

    Site Health page now presents a much more realistic test about object caching.

--- init.php
+++ init.php
@@ -12,7 +12,7 @@ define( 'POKM_PRODUCT_URL', 'https://github.com/Pierre-Lannoy/wp-keys-master' );
 define( 'POKM_PRODUCT_SHORTNAME', 'keys-master' );
 define( 'POKM_PRODUCT_ABBREVIATION', 'keysmaster' );
 define( 'POKM_SLUG', 'keys-master' );
-define( 'POKM_VERSION', '1.5.1' );
+define( 'POKM_VERSION', '1.6.0-dev0' );
 define( 'POKM_CODENAME', '"-"' );
 define( 'POKM_CDN_AVAILABLE', true );
 
